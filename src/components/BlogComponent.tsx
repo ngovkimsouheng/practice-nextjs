ff20c5dacfdc5196f55296e39f8bf7148a2fdc62
ngovkimsouheng <kokocorona9@gmail.com>
2026-05-19 23:46:31 +0700

    re-design card

--- src/components/BlogComponent.tsx
+++ src/components/BlogComponent.tsx
@@ -9,10 +9,10 @@ interface BlogType {
 export default function BlogComponent({ id, title, body }: BlogType) {
   return (
     <Link href={`/blog/${id}`}>
-      <div className="p-6 mt-8 shadow-sm cursor-pointer hover:ring-1 hover:ring-blue-500 border max-w-7xl mx-auto rounded-lg">
-        <h1 className="text-2xl text-shadow-blue-700 font-bold">{title}</h1>
+      <div className="p-6  shadow-sm cursor-pointer hover:ring-1 hover:ring-blue-500 border max-w-7xl mx-auto rounded-lg">
+        <h1 className="text-2xl text-shadow-blue-700 line-clamp-2 font-bold">{title}</h1>
         <br />
-        <p className="text-[20px] text-gray-600">{body}</p>
+        <p className="text-[20px] text-gray-600 line-clamp-3">{body}</p>
       </div>
     </Link>
   );
